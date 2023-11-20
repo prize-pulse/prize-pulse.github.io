af328862015170b1127fd3ac925584b46af1bfad
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-21 00:03:48 +0530

    Update giveaway page ff.js

--- js/giveaway page ff.js
+++ js/giveaway page ff.js
@@ -5,15 +5,22 @@ var timeS = document.getElementById("timeRem");
 
 
 fetch("https://prize-pulse.uk.to/api/data.ff.giveaway", {
-method: 'GET',
+method: 'GET', // or 'GET' or any other HTTP method
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    // add any other headers if needed
   }
 }).then(data => data.json()).then(data => {
 if(!data || !data.success){
  prizeS.innerHTML="Error When Fetching.!"; 
 timeS.innerHTML = "Error When Fetching.!";
 }
+
+var prizeF = data.prize ? data.prize : "Error When Fetching.!";
+var timeF = data.time ? data.time : "Error When Fetching.!";
+
+prizeS.innerHTML = prizeF;
+timeS.innerHTML = timeF;
   
 }).catch(er => {
 });
