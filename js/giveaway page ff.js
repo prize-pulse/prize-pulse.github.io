3f53d9b12bb0bc37dc1c61c21545f1f6b0b63d43
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-20 23:21:21 +0530

    Update giveaway page ff.js

--- js/giveaway page ff.js
+++ js/giveaway page ff.js
@@ -1,1 +1,10 @@
 //script
+var prizeS = document.getElementById("prize");
+var timeS = document.getElementById("timeRem");
+//var titleS = document.getElementById("ttl");
+
+
+fetch("https://prize-pulse.uk.to/api/state.ff.giveaway").then(data => data.json()).then(data => {
+
+}).catch(er => {
+});
