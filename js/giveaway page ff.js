a3f4c7278ed8a085e783016748b4fce13d344e1d
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-21 13:39:29 +0530

    Update giveaway page ff.js

--- js/giveaway page ff.js
+++ js/giveaway page ff.js
@@ -12,15 +12,15 @@ method: 'GET', // or 'GET' or any other HTTP method
   }
 }).then(data => data.json()).then(data => {
 if(!data || !data.success){
- prizeS.innerHTML="Error When Fetching.!"; 
-timeS.innerHTML = "Error When Fetching.!";
+ prizeS.innerHTML="Prize: Error When Fetching.!"; 
+timeS.innerHTML = "Time Remaining: Error When Fetching.!";
 }
 
 var prizeF = data.prize ? data.prize : "Error When Fetching.!";
 var timeF = data.time ? data.time : "Error When Fetching.!";
 
-prizeS.innerHTML = prizeF;
-timeS.innerHTML = timeF;
+prizeS.innerHTML = `Prize: ${prizeF}`;
+timeS.innerHTML = `Time Remaining: ${timeF}`;
   
 }).catch(er => {
 });
