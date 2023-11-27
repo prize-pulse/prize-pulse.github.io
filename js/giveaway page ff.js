8d41cbd7c8f529b93fa63536e9d13c1ac95cf69e
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-27 23:32:41 +0530

    Update giveaway page ff.js

--- js/giveaway page ff.js
+++ js/giveaway page ff.js
@@ -2,6 +2,7 @@
 var prizeS = document.getElementById("prize");
 var timeS = document.getElementById("timeRem");
 var userCountS = document.getElementById("user_count");
+let msTimeC;
 //var timeS = document.getElementById("timeRem");
 //var titleS = document.getElementById("ttl");
 //{"success":true,"data":{"prize":null,"winner":null,"time_left":115480,"usersCount":11},"button_type":"join"}
@@ -17,6 +18,7 @@ if(!data || !data.success){
   userCountS.innerHTML = "Total Participants: 0";
  prizeS.innerHTML="Prize: Error When Fetching.!"; 
 timeS.innerHTML = "Time Remaining: Error When Fetching.!";
+  return;
 }
 
 var prizeF = data.data.prize ? data.data.prize : "Not Provided";
@@ -25,6 +27,13 @@ var userCountF = data.data.usersCount ? data.data.usersCount : 0;
 prizeS.innerHTML = `Prize: ${prizeF}`;
 timeS.innerHTML = `Time Remaining: ${timeF}`;
 userCountS.innerHTML = `Total Participants: ${userCountF}`;
-  
+msTimeC = data.data.time_left;
+setInterval(() => {
+msTimeC = msTimeC - 1000;
+if(msTimeC < 0){
+  timeS.innerHTML = `Time Remaining: 0 D, 0 H, 0 M, 0 S;`;
+  return;
+}
+}, 1000);
 }).catch(er => {
 });
