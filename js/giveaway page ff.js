0bc3114a05760c849e72fdef8fa8dd2bfc38b818
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2023-11-27 23:12:02 +0530

    Update giveaway page ff.js

--- js/giveaway page ff.js
+++ js/giveaway page ff.js
@@ -1,8 +1,10 @@
 //script
 var prizeS = document.getElementById("prize");
 var timeS = document.getElementById("timeRem");
+var userCountS = document.getElementById("user_count");
+//var timeS = document.getElementById("timeRem");
 //var titleS = document.getElementById("ttl");
-
+//{"success":true,"data":{"prize":null,"winner":null,"time_left":115480,"usersCount":11},"button_type":"join"}
 
 fetch("https://prize-pulse.uk.to/api/data.ff.giveaway", {
 method: 'GET', // or 'GET' or any other HTTP method
@@ -12,15 +14,17 @@ method: 'GET', // or 'GET' or any other HTTP method
   }
 }).then(data => data.json()).then(data => {
 if(!data || !data.success){
+  userCountS.innerHTML = "Total Participants: 0";
  prizeS.innerHTML="Prize: Error When Fetching.!"; 
 timeS.innerHTML = "Time Remaining: Error When Fetching.!";
 }
 
-var prizeF = data.prize ? data.prize : "Error When Fetching.!";
-var timeF = data.time ? data.time : "Error When Fetching.!";
-
+var prizeF = data.data.prize ? data.data.prize : "Not Provided";
+var timeF = data.data.time_left ? data.data.time_left : "0";
+var userCountF = data.data.usersCount ? data.data.usersCount : 0;
 prizeS.innerHTML = `Prize: ${prizeF}`;
 timeS.innerHTML = `Time Remaining: ${timeF}`;
+userCountS.innerHTML = `Total Participants: ${userCountF}`;
   
 }).catch(er => {
 });
